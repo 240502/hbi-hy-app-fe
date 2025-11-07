53708776fc0104722dc108d4ba28dc78adabfb46
Sang <Nguyen.Sang@hanes.com>
2025-11-07 13:19:35 +0700

    first commit

---
+++ projects/main-app/src/app/data-access/api/question.service.ts
@@ -0,0 +1,10 @@
+import { Injectable,signal } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Question } from "@models/question";
+
+
+@Injectable({ providedIn: "root" })
+
+export class QuestionService{
+    
+}
---
+++ projects/main-app/src/app/models/question.ts
@@ -0,0 +1,4 @@
+export interface Question {
+  questionId: number;
+  content: string;
+}
